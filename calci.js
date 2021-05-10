6239a29f764744cc08f30bb45e83bdf93858226d
lokita282 <lokitavarma282@gmail.com>
2021-05-10 11:11:52 +0530

    initialise

---
+++ calci.js
@@ -0,0 +1,29 @@
+//Define UI vars 
+const form = document.querySelector('#ap')
+const firstTerm = document.querySelector('#first-term')
+const commonDifference = document.querySelector('#common-difference')
+const n = document.querySelector('#n')
+const results = document.querySelector('#results')
+
+loadEventListeners();
+
+function loadEventListeners(){
+  form.addEventListener('submit', calculate);
+}
+
+//calculate
+function calculate(e){
+  if(firstTerm.value === '' || commonDifference.value === '' || n.value === ''){
+    alert("Add required details")
+  }
+  
+  let nthTerm = firstTerm + n*commonDifference - commonDifference
+  console.log(nthTerm)
+  console.log(firstTerm.value)
+  console.log(n.value)
+  console.log(commonDifference.value)
+  return nthTerm;
+
+  e.preventDefault();
+}
+console.log(calculate)
