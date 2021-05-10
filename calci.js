80de1c7f38e48a8b30787a44ca41e234a12c55c7
lokita282 <lokitavarma282@gmail.com>
2021-05-10 20:06:21 +0530

    comment error

--- calci.js
+++ calci.js
@@ -1,29 +1,123 @@
-//Define UI vars 
-const form = document.querySelector('#ap')
-const firstTerm = document.querySelector('#first-term')
+//Define UI vars for AP
+const formAp = document.querySelector('#ap')
+const firstTermAp = document.querySelector('#first-term-ap')
 const commonDifference = document.querySelector('#common-difference')
-const n = document.querySelector('#n')
-const results = document.querySelector('#results')
+const nAp = document.querySelector('#n-ap')
+const resultsAp = document.querySelector('#resultsAp')
 
 loadEventListeners();
 
 function loadEventListeners(){
-  form.addEventListener('submit', calculate);
+  formAp.addEventListener('submit', calculateAp);
+
 }
 
-//calculate
-function calculate(e){
+calculate
+function calculateAp(e){
   e.preventDefault();
-  if(firstTerm.value === '' || commonDifference.value === '' || n.value === ''){
+  calculateNthTermAp();
+  calculateSumAp();
+}
+
+function calculateNthTermAp(e){
+  //e.preventDefault();
+  if(firstTermAp.value === '' || commonDifference.value === '' || nAp.value === ''){
     alert("Add required details")
   }
-  let a = Number(firstTerm.value)
+  let aAp = Number(firstTermAp.value)
   let d = Number(commonDifference.value);
-  let N = Number(n.value)
+  let NAp = Number(nAp.value)
 
-  let nthTerm = a+((N-1)*d)
- console.log(nthTerm);
+  let nthTermAp = aAp +((NAp-1)*d)
+ const li = document.createElement('li');
+ li.className = 'collection-item'
+ li.appendChild(document.createTextNode(`the nth term is ${nthTermAp}`));
 
+ resultsAp.appendChild(li);
+}
+
+function calculateSumAp(e){
+  //e.preventDefault();
+  if(firstTermAp.value === '' || commonDifference.value === '' || nAp.value === ''){
+    alert("Add required details")
+  }
+  let aAp = Number(firstTermAp.value)
+  let d = Number(commonDifference.value);
+  let NAp = Number(nAp.value)
+
+  let sumAp = ((NAp)*((2*aAp)+(NAp-1)*d))/2
+ const li = document.createElement('li');
+ li.className = 'collection-item'
+ li.appendChild(document.createTextNode(`the sum is ${sumAp}`));
+
+ resultsAp.appendChild(li);
+}
+
+
+
+//Define UI vars for GP
+const formGp = document.querySelector('#gp')
+const firstTermGp = document.querySelector('#first-term-gp')
+const commonRatio = document.querySelector('#common-ratio')
+const nGp = document.querySelector('#n-gp')
+const resultsGp = document.querySelector('#resultsGp')
+
+loadEventListeners();
+
+function loadEventListeners(){
+  formGp.addEventListener('submit', calculateGp);
 
 }
 
+function calculateGp(e){
+  e.preventDefault();
+  calculateNthTermGp();
+  calculateSumGp();
+}
+
+function calculateNthTermGp(e){
+  //e.preventDefault();
+  if(firstTermGp.value === '' || commonRatio.value === '' || nGp.value === ''){
+    alert("Add required details")
+  }
+  let aGp = Number(firstTermGp.value)
+  let r = Number(commonRatio.value);
+  let NGp = Number(nGp.value)
+
+  let x = NGp-1;
+  let y = Math.pow(r,x)
+  let nthTermGp = (aGp * y)
+  console.log(x);
+  console.log(y);
+  console.log(nthTermGp);
+ const li = document.createElement('li');
+ li.className = 'collection-item'
+ li.appendChild(document.createTextNode(`the nth term is ${nthTermGp}`));
+
+ resultsGp.appendChild(li);
+}
+
+function calculateSumGp(e){
+  //e.preventDefault();
+  if(firstTermGp.value === '' || commonRatio.value === '' || nGp.value === ''){
+    alert("Add required details")
+  }
+  let aGp = Number(firstTermGp.value)
+  let r = Number(commonRatio.value);
+  let NGp = Number(nGp.value)
+  let sumGp;
+
+  if(r<1){
+    let v = Math.pow(r,NGp) - 1 
+    sumGp = (aGp * v) / (r-1)
+  }
+   else if(r>1){
+    let v = Math.pow(r,NGp) - 1 
+    sumGp = (aGp * v) / (r-1)
+  }
+ const li = document.createElement('li');
+ li.className = 'collection-item'
+ li.appendChild(document.createTextNode(`the sum is ${sumGp}`));
+
+ resultsGp.appendChild(li);
+}
